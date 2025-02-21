c0faba6fc4f4650df2613bed4b0c8f7334c4884a
Aaron Lampert <therebelcreeper@gmail.com>
2025-02-21 16:55:55 -0600

    Now uses pcre2 for regex

---
+++ README.md
@@ -0,0 +1,9 @@
+# TF2-Demo-Renamer
+Parses demo files and adds the map name to the end of the file name
+To install just drag the .exe file to the same directory as the demo files. Ideally used with valve auto recording, supports prefix.
+### Example
+`rebel_2024-08-16_22-39-43.dem` will be renamed to `rebel_2024-08-16_22-39-43_cp_sunshine.dem`
+
+
+## How to build
+Requires https://pcre2project.github.io/pcre2/
--- main.c
+++ main.c
@@ -1,5 +1,8 @@
+#define PCRE2_STATIC
+#define PCRE2_CODE_UNIT_WIDTH 8
 #include <stdio.h>
 #include <dirent.h>
+#include "pcre2.h"
 #include "memmem.h"
 
 int indexOfBin(char *str, int len1, const char *substr, int len2)
@@ -22,6 +25,23 @@ int indexOf(const char *str, const char *substr)
 	return result - str;
 }
 
+int isValidDemo(char *fileName)
+{
+	const char *pattern = "([a-zA-Z]*)(_?)(\\d{4}-\\d{2}-\\d{2})_(\\d{2}-\\d{2}-\\d{2})(\\.dem)";
+	const unsigned char *src = (unsigned char*)fileName;
+	int error_number;
+	PCRE2_SIZE error_offset;
+	pcre2_code *re = pcre2_compile_8((unsigned char*)pattern, PCRE2_ZERO_TERMINATED, 0, &error_number, &error_offset, NULL);
+	if (re == NULL)
+	{
+		return -1;
+	}
+	pcre2_match_data *match_data = pcre2_match_data_create_from_pattern(re, NULL);
+	int rc = pcre2_match(re, src, strlen(fileName), 0, 0, match_data, NULL);
+	pcre2_match_data_free(match_data);
+	pcre2_code_free(re);
+	return rc;
+}
 
 void getMapName(char *mapName, char *fileName)
 {
@@ -36,8 +56,10 @@ void getMapName(char *mapName, char *fileName)
 			int end = indexOfBin(buff, sizeof(buff), ".bsp", 4);
 			snprintf(mapName, end - start + 1, "%s", buff + start);
 			fclose(f);
+			return;
 		}
 	}
+	sprintf(mapName, "error");
 	fclose(f);
 }
 
@@ -64,24 +86,30 @@ int main(int argc, char *argv[])
 		char mapName[128];
 		char *fullFileName = fileList->d_name;
 		int i;
+		char c;
 		
-		if ((i = indexOf(fullFileName, ".dem")) != -1)
+		if ((i = indexOf(fullFileName, ".dem")) != -1 && isValidDemo(fullFileName) == 6)
 		{
 			// Parse demo file for map name
 			getMapName(mapName, fullFileName);
 			
 			// Add map name before file extension
+			c = fullFileName[i];
 			fullFileName[i] = '\0';
 			sprintf(newFileName, "%s_%s.dem", fullFileName, mapName);
+			fullFileName[i] = c;
 			
 			// Rename file
 			//if (rename(fullFileName, newFileName) != 0)
 			//{
-			//	perror("Failed to rename file.");
+			//	perror("Failed to rename file");
 			//}	
 			
-			printf("Found file: %s with map name %s\n", fullFileName, mapName);
-			printf("New file name: %s\n", newFileName);
+			printf("Renaming %s to %s\n", fullFileName, newFileName);
+		}
+		else
+		{
+			printf("Skipping %s\n", fullFileName);
 		}
 	}
 	closedir(d);
